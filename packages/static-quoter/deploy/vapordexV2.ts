ba92a9a60049ba92fc05dfcd49ac3c777a0093c7
MejiasDev <jose@mejiasdev.com>
2024-10-29 15:25:58 +0100

    release: deploy to apechain

--- packages/static-quoter/deploy/vapordexV2.ts
+++ packages/static-quoter/deploy/vapordexV2.ts
@@ -1,50 +1,59 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
+import type { DeployFunction } from "hardhat-deploy/types";
+import type { HardhatRuntimeEnvironment } from "hardhat/types";
 import addresses from "../constants/addresses.json";
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts, network } = hre;
-  const { deploy, log } = deployments;
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+	const { deployments, getNamedAccounts, network } = hre;
+	const { deploy, log } = deployments;
 
-  const allowedNetworks = ["avalanche", "fuji", "telos", "telosTestnet"];
-  if (!allowedNetworks.includes(network.name))
-    throw new Error(`Wrong network! Only "${allowedNetworks}" supported`);
+	const allowedNetworks = [
+		"avalanche",
+		"fuji",
+		"telos",
+		"telosTestnet",
+		"apechain",
+	];
+	if (!allowedNetworks.includes(network.name))
+		throw new Error(`Wrong network! Only "${allowedNetworks}" supported`);
 
-  const contractName = "UniswapV3StaticQuoter";
-  let factory;
-  switch (network.name) {
-    case "fuji":
-      factory = addresses.fuji.protocols.vapordexV2.factory;
-      break;
+	const contractName = "UniswapV3StaticQuoter";
+	let factory;
+	switch (network.name) {
+		case "fuji":
+			factory = addresses.fuji.protocols.vapordexV2.factory;
+			break;
 
-    case "avalanche":
-      factory = addresses.avalanche.protocols.vapordexV2.factory;
-      break;
-    case "telosTestnet":
-      factory = addresses.telosTestnet.protocols.vapordexV2.factory;
-      break;
-    case "telos":
-      factory = addresses.telos.protocols.vapordexV2.factory;
-      break;
-  }
-  const args = [factory];
-  const { deployer } = await getNamedAccounts();
+		case "avalanche":
+			factory = addresses.avalanche.protocols.vapordexV2.factory;
+			break;
+		case "telosTestnet":
+			factory = addresses.telosTestnet.protocols.vapordexV2.factory;
+			break;
+		case "telos":
+			factory = addresses.telos.protocols.vapordexV2.factory;
+			break;
+		case "apechain":
+			factory = addresses.apechain.protocols.vapordexV2.factory;
+			break;
+	}
+	const args = [factory];
+	const { deployer } = await getNamedAccounts();
 
-  log("1) Deploy contract");
-  const deployResult: any = await deploy(contractName, {
-    from: deployer,
-    contract: contractName,
-    skipIfAlreadyDeployed: false,
-    log: true,
-    args,
-  });
+	log("1) Deploy contract");
+	const deployResult: any = await deploy(contractName, {
+		from: deployer,
+		contract: contractName,
+		skipIfAlreadyDeployed: false,
+		log: true,
+		args,
+	});
 
-  if (deployResult.newlyDeployed)
-    log(`- 🎉 Deployed at: ${deployResult.address}`);
-  else
-    log(
-      `- ⏩ Deployment skipped, using previous deployment at: ${deployResult.address}`
-    );
+	if (deployResult.newlyDeployed)
+		log(`- 🎉 Deployed at: ${deployResult.address}`);
+	else
+		log(
+			`- ⏩ Deployment skipped, using previous deployment at: ${deployResult.address}`,
+		);
 };
 
 export default func;
--- packages/static-quoter/hardhat.config.ts
+++ packages/static-quoter/hardhat.config.ts
@@ -15,146 +15,159 @@ const DEPLOYER_ADDRESS = process.env.DEPLOYER_ADDRESS as string;
 const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY as string;
 const ETHERSCAN_API_KEY = process.env.SNOWTRACE_API_KEY;
 const DOGECHAIN_RPC = process.env.DOGECHAIN_RPC ?? "";
+const APECHAIN_RPC =
+	process.env.APECHAIN_RPC ?? "https://rpc.apechain.com/http";
 
 const avalancheConfig = {
-  url: AVALANCHE_RPC,
-  chainId: 43114,
-  live: true,
-  saveDeployments: true,
-  accounts: [] as string[],
+	url: AVALANCHE_RPC,
+	chainId: 43114,
+	live: true,
+	saveDeployments: true,
+	accounts: [] as string[],
 };
 
 const fujiConfig = {
-  url: FUJI_RPC,
-  chainId: 43113,
-  live: true,
-  saveDeployments: true,
-  accounts: [] as string[],
+	url: FUJI_RPC,
+	chainId: 43113,
+	live: true,
+	saveDeployments: true,
+	accounts: [] as string[],
 };
 
 const telosTestnetConfig = {
-  url: TELOS_TESTNET_RPC,
-  chainId: 41,
-  live: true,
-  saveDeployments: true,
-  accounts: [] as string[],
+	url: TELOS_TESTNET_RPC,
+	chainId: 41,
+	live: true,
+	saveDeployments: true,
+	accounts: [] as string[],
 };
 
 const telosConfig = {
-  url: TELOS_RPC,
-  chainId: 40,
-  live: true,
-  saveDeployments: true,
-  accounts: [] as string[],
+	url: TELOS_RPC,
+	chainId: 40,
+	live: true,
+	saveDeployments: true,
+	accounts: [] as string[],
 };
 
 const dogechainConfig = {
-  url: DOGECHAIN_RPC,
-  chainId: 2000,
-  live: true,
-  saveDeployments: true,
-  accounts: [] as string[],
+	url: DOGECHAIN_RPC,
+	chainId: 2000,
+	live: true,
+	saveDeployments: true,
+	accounts: [] as string[],
+};
+
+const apechainConfig = {
+	url: APECHAIN_RPC,
+	chainId: 33139,
+	live: true,
+	saveDeployments: true,
+	accounts: [] as string[],
 };
 
 if (DEPLOYER_PRIVATE_KEY) {
-  avalancheConfig.accounts.push(DEPLOYER_PRIVATE_KEY);
-  fujiConfig.accounts.push(DEPLOYER_PRIVATE_KEY);
-  telosTestnetConfig.accounts.push(DEPLOYER_PRIVATE_KEY);
-  telosConfig.accounts.push(DEPLOYER_PRIVATE_KEY);
-  dogechainConfig.accounts.push(DEPLOYER_PRIVATE_KEY);
+	avalancheConfig.accounts.push(DEPLOYER_PRIVATE_KEY);
+	fujiConfig.accounts.push(DEPLOYER_PRIVATE_KEY);
+	telosTestnetConfig.accounts.push(DEPLOYER_PRIVATE_KEY);
+	telosConfig.accounts.push(DEPLOYER_PRIVATE_KEY);
+	dogechainConfig.accounts.push(DEPLOYER_PRIVATE_KEY);
+	apechainConfig.accounts.push(DEPLOYER_PRIVATE_KEY);
 }
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.7.6",
-    settings: {
-      evmVersion: "istanbul",
-      optimizer: {
-        enabled: true,
-        runs: 1_000_000,
-      },
-      metadata: {
-        bytecodeHash: "none",
-      },
-    },
-  },
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      forking: { enabled: true, url: "https://mainnet.telos.net/evm" },
-    },
-    avalanche: avalancheConfig,
-    fuji: fujiConfig,
-    telosTestnet: telosTestnetConfig,
-    telos: telosConfig,
-    dogechain: dogechainConfig,
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-      1: DEPLOYER_ADDRESS,
-      10: DEPLOYER_ADDRESS,
-      42161: DEPLOYER_ADDRESS,
-      43114: DEPLOYER_ADDRESS,
-      43113: DEPLOYER_ADDRESS,
-      40: DEPLOYER_ADDRESS,
-      41: DEPLOYER_ADDRESS,
-    },
-  },
-  abiExporter: {
-    path: "./abis",
-    clear: true,
-    flat: true,
-    only: [":UniswapV3StaticQuoter"],
-  },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-    customChains: [
-      {
-        network: "mainnet",
-        chainId: 1,
-        urls: {
-          apiURL: "https://api.etherscan.io/api",
-          browserURL: "https://etherscan.io",
-        },
-      },
-      {
-        network: "optimism",
-        chainId: 10,
-        urls: {
-          apiURL: "https://api-optimistic.etherscan.io/api",
-          browserURL: "https://optimistic.etherscan.io",
-        },
-      },
-      {
-        network: "arbitrum",
-        chainId: 42161,
-        urls: {
-          apiURL: "https://api.arbiscan.io/api",
-          browserURL: "https://arbiscan.io",
-        },
-      },
-      {
-        network: "avalanche",
-        chainId: 43114,
-        urls: {
-          apiURL: "https://api.snowtrace.io/api",
-          browserURL: "https://snowtrace.io",
-        },
-      },
-      {
-        network: "polygon",
-        chainId: 137,
-        urls: {
-          apiURL: "https://api.polygonscan.com/api",
-          browserURL: "https://polygonscan.com",
-        },
-      },
-    ],
-  },
-  mocha: {
-    timeout: 600000000,
-  },
+	solidity: {
+		version: "0.7.6",
+		settings: {
+			evmVersion: "istanbul",
+			optimizer: {
+				enabled: true,
+				runs: 1_000_000,
+			},
+			metadata: {
+				bytecodeHash: "none",
+			},
+		},
+	},
+	defaultNetwork: "hardhat",
+	networks: {
+		hardhat: {
+			forking: { enabled: true, url: "https://mainnet.telos.net/evm" },
+		},
+		avalanche: avalancheConfig,
+		fuji: fujiConfig,
+		telosTestnet: telosTestnetConfig,
+		telos: telosConfig,
+		dogechain: dogechainConfig,
+		apechain: apechainConfig,
+	},
+	namedAccounts: {
+		deployer: {
+			default: 0,
+			1: DEPLOYER_ADDRESS,
+			10: DEPLOYER_ADDRESS,
+			42161: DEPLOYER_ADDRESS,
+			43114: DEPLOYER_ADDRESS,
+			43113: DEPLOYER_ADDRESS,
+			40: DEPLOYER_ADDRESS,
+			41: DEPLOYER_ADDRESS,
+			33139: DEPLOYER_ADDRESS,
+		},
+	},
+	abiExporter: {
+		path: "./abis",
+		clear: true,
+		flat: true,
+		only: [":UniswapV3StaticQuoter"],
+	},
+	etherscan: {
+		apiKey: ETHERSCAN_API_KEY,
+		customChains: [
+			{
+				network: "mainnet",
+				chainId: 1,
+				urls: {
+					apiURL: "https://api.etherscan.io/api",
+					browserURL: "https://etherscan.io",
+				},
+			},
+			{
+				network: "optimism",
+				chainId: 10,
+				urls: {
+					apiURL: "https://api-optimistic.etherscan.io/api",
+					browserURL: "https://optimistic.etherscan.io",
+				},
+			},
+			{
+				network: "arbitrum",
+				chainId: 42161,
+				urls: {
+					apiURL: "https://api.arbiscan.io/api",
+					browserURL: "https://arbiscan.io",
+				},
+			},
+			{
+				network: "avalanche",
+				chainId: 43114,
+				urls: {
+					apiURL: "https://api.snowtrace.io/api",
+					browserURL: "https://snowtrace.io",
+				},
+			},
+			{
+				network: "polygon",
+				chainId: 137,
+				urls: {
+					apiURL: "https://api.polygonscan.com/api",
+					browserURL: "https://polygonscan.com",
+				},
+			},
+		],
+	},
+	mocha: {
+		timeout: 600000000,
+	},
 };
 
 export default config;
