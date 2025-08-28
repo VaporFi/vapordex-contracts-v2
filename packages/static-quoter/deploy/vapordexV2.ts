c1fc64ce41e3fe04f852e6ef6163f3f3370eee29
ihiteshagrawal <hiteshagarwal172@gmail.com>
2025-08-28 17:39:51 +0530

    chore: deploy base sepolia

--- packages/static-quoter/deploy/vapordexV2.ts
+++ packages/static-quoter/deploy/vapordexV2.ts
@@ -2,66 +2,70 @@ import type { DeployFunction } from "hardhat-deploy/types";
 import type { HardhatRuntimeEnvironment } from "hardhat/types";
 import addresses from "../constants/addresses.json";
 
+const allowedNetworks = [
+  "avalanche",
+  "fuji",
+  "telos",
+  "telosTestnet",
+  "apechain",
+  "europaTestnet",
+  "curtis",
+  "baseSepolia",
+];
+
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-	const { deployments, getNamedAccounts, network } = hre;
-	const { deploy, log } = deployments;
+  const { deployments, getNamedAccounts, network } = hre;
+  const { deploy, log } = deployments;
 
-	const allowedNetworks = [
-		"avalanche",
-		"fuji",
-		"telos",
-		"telosTestnet",
-		"apechain",
-		"europaTestnet",
-		"curtis",
-	];
-	if (!allowedNetworks.includes(network.name))
-		throw new Error(`Wrong network! Only "${allowedNetworks}" supported`);
+  if (!allowedNetworks.includes(network.name))
+    throw new Error(`Wrong network! Only "${allowedNetworks}" supported`);
 
-	const contractName = "UniswapV3StaticQuoter";
-	let factory;
-	switch (network.name) {
-		case "fuji":
-			factory = addresses.fuji.protocols.vapordexV2.factory;
-			break;
+  const contractName = "UniswapV3StaticQuoter";
+  let factory;
+  switch (network.name) {
+    case "baseSepolia":
+      factory = addresses.baseSepolia.protocols.vapordexV2.factory;
+      break;
+    case "fuji":
+      factory = addresses.fuji.protocols.vapordexV2.factory;
+      break;
+    case "avalanche":
+      factory = addresses.avalanche.protocols.vapordexV2.factory;
+      break;
+    case "telosTestnet":
+      factory = addresses.telosTestnet.protocols.vapordexV2.factory;
+      break;
+    case "telos":
+      factory = addresses.telos.protocols.vapordexV2.factory;
+      break;
+    case "apechain":
+      factory = addresses.apechain.protocols.vapordexV2.factory;
+      break;
+    case "europaTestnet":
+      factory = addresses.europaTestnet.protocols.vapordexV2.factory;
+      break;
+    case "curtis":
+      factory = addresses.curtis.protocols.vapordexV2.factory;
+      break;
+  }
+  const args = [factory];
+  const { deployer } = await getNamedAccounts();
 
-		case "avalanche":
-			factory = addresses.avalanche.protocols.vapordexV2.factory;
-			break;
-		case "telosTestnet":
-			factory = addresses.telosTestnet.protocols.vapordexV2.factory;
-			break;
-		case "telos":
-			factory = addresses.telos.protocols.vapordexV2.factory;
-			break;
-		case "apechain":
-			factory = addresses.apechain.protocols.vapordexV2.factory;
-			break;
-		case "europaTestnet":
-			factory = addresses.europaTestnet.protocols.vapordexV2.factory;
-			break;
-		case "curtis":
-			factory = addresses.curtis.protocols.vapordexV2.factory;
-			break;
-	}
-	const args = [factory];
-	const { deployer } = await getNamedAccounts();
+  log("1) Deploy contract");
+  const deployResult: any = await deploy(contractName, {
+    from: deployer,
+    contract: contractName,
+    skipIfAlreadyDeployed: false,
+    log: true,
+    args,
+  });
 
-	log("1) Deploy contract");
-	const deployResult: any = await deploy(contractName, {
-		from: deployer,
-		contract: contractName,
-		skipIfAlreadyDeployed: false,
-		log: true,
-		args,
-	});
-
-	if (deployResult.newlyDeployed)
-		log(`- 🎉 Deployed at: ${deployResult.address}`);
-	else
-		log(
-			`- ⏩ Deployment skipped, using previous deployment at: ${deployResult.address}`,
-		);
+  if (deployResult.newlyDeployed)
+    log(`- 🎉 Deployed at: ${deployResult.address}`);
+  else
+    log(
+      `- ⏩ Deployment skipped, using previous deployment at: ${deployResult.address}`
+    );
 };
 
 export default func;
